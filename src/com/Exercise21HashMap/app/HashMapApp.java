732c19ebb2a3e14897b832bb187aa2290ff18909
Mabestron <ivangamaliel555@hotmail.com>
2019-10-23 13:07:40 -0500

    Hash Map v2

--- src/com/Exercise21HashMap/app/HashMapApp.java
+++ src/com/Exercise21HashMap/app/HashMapApp.java
@@ -1,11 +1,30 @@
 package com.Exercise21HashMap.app;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapApp 
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		HashMap<Integer,String> miHash =new HashMap<Integer,String>();
+		miHash.put(1,"Ary");
+		miHash.put(2,"Cristina");
+		
+		
+		for(int i=0;i<=miHash.size();i++)
+		{
+			if(miHash.containsKey(i))
+			{
+				System.out.println(miHash.get(i));
+			}
+		}
+		
+		for(Map.Entry<Integer, String> miMap :miHash.entrySet())
+		{
+			System.out.println(String.format("The key is: %d, value: %s", miMap.getKey(),miMap.getValue()));
+		}
+		
 
 	}
 
